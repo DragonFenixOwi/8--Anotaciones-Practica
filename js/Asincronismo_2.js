134087b7c40420289556e0c75de6f225b8dbe0f3
DragonFenixOwi <mecaowi@gmail.com>
2020-06-02 01:16:24 +0200

    JQuery de nuevo, API_URL

--- js/Asincronismo_2.js
+++ js/Asincronismo_2.js
@@ -17,39 +17,34 @@
 
 
 // Hacemos una API_URL no va a cambair
-const API_URL= "https://swapi.dev/api";
-const PERSONAJES_URL = "people/:id:"; 
+const API_URL= "https://swapi.dev/api/";
+const PERSONAJES_URL = "people/:id"; 
 const OPCIONES = {crossDomain: true}; 
 
 
-// e
+
+
 const PERSONAJE = function (personaje)
     {
         // c "https://swapi.dev/ " 
-        document.write(`Hola yo soy ${personaje.name} ${"<br><br>"}`);
+            document.write(`Hola mi nombre es ${personaje.name}${"<br><br>"}`);
+
     };
-
-
 
 
 
 
 function perdirPersonaje (id)
     {
-        const URL = `${API_URL}${PERSONAJE_URL.replace(":id",id)}`; 
-        $.get(LEIA_URL, OPCIONES,PERSONAJE);
+        const URL = `${API_URL}${PERSONAJES_URL.replace(":id",id)}`; 
+        $.get(URL, OPCIONES,PERSONAJE);
     }
 
 
 
 //llamamos a la API
-pediPersonaje(1);
-pediPersonaje(2);
-pediPersonaje(3);
-/*
-    -Las API
-        -No podemos controlar 
-        - Porque pedimos a la API una petición pero no pudimos controlar el orden que yo quiero que llegue los valores
+perdirPersonaje(1);
+perdirPersonaje(2);
+perdirPersonaje(3);
 
-*/
 
